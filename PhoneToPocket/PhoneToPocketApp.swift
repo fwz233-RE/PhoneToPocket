9281c29de8c9107946efe47c0e3df323466356b6
fwz233 <1041871038@qq.com>
2026-02-24 14:54:46 +0800

    Initial Commit

---
+++ PhoneToPocket/PhoneToPocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneToPocketApp.swift
+//  PhoneToPocket
+//
+//  Created by Mountain Celeste on 2026/2/24.
+//
+
+import SwiftUI
+
+@main
+struct PhoneToPocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
